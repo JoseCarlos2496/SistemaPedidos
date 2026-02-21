f1fb68107b3dde258a5eaf045827af22f145d6d8
Jose Araujo <jose_araujom@outlook.com>
2026-02-21 08:42:03 -0500

    Se agrega script de selects
    - Se agrega script de selects

---
+++ SQL/Script_Select.sql
@@ -0,0 +1,16 @@
+
+USE SistemaPedido;
+GO
+
+-- Tabla: PedidoCabecera
+SELECT * FROM dbo.PedidoCabecera 
+GO
+
+-- Tabla: PedidoDetalle
+SELECT * FROM dbo.PedidoDetalle
+GO
+
+-- Tabla: LogAuditoria
+SELECT * FROM dbo.LogAuditoria
+GO
+
